356561d3033d23ae7d7ba3ac8f7309486df4fd93
Anai.ds <s2322073@stu.musashino-u.ac.jp>
2024-01-27 17:26:57 +0900

    モデル再構築

--- cluster/forms.py
+++ cluster/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 
 class DataInputForm(forms.Form):
-    売上高成長率平均 = forms.FloatField(label='売上高成長率3年平均')
-    営業利益率平均 = forms.FloatField(label='営業利益率3年平均')
-    ROE = forms.FloatField(label='ROE')
-    粗利率 = forms.FloatField(label='粗利率')
+    売上高 = forms.FloatField(label='売上高成長率3年平均')
+    営業利益 = forms.FloatField(label='営業利益率3年平均')
+    roe = forms.FloatField(label='ROE')
     有形固定資産_純資産 = forms.FloatField(label='有形固定資産 / 純資産')
     固定負債_純資産 = forms.FloatField(label='固定負債 / 純資産')
     自己資本比率 = forms.FloatField(label='自己資本比率')
-    PBR = forms.FloatField(label='PBR')
+    pbr = forms.FloatField(label='PBR')
+    営業利益率 = forms.FloatField(label='営業利益率')
     株価上昇率_3年 = forms.FloatField(label='株価上昇率(2020~2023)')
