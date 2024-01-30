b3a9dd2011409c6a51ba08c6a0f62c25594fa817
Anai.ds <s2322073@stu.musashino-u.ac.jp>
2024-01-31 03:58:56 +0900

    更新

--- company/admin.py
+++ company/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Company
 
-admin.site.register(Company)
+class Search(admin.ModelAdmin):
+    
+    search_fields = ['会社名']
+
+admin.site.register(Company, Search)
 
 # Register your models here.
